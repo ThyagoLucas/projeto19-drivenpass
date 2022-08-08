737b748ef6298ba54e4003e2d3a134cda232c5c3
pedro imbrech <elprogramadorpedro@gmail.com>
2022-08-08 11:51:05 -0300

    rev

--- src/configs/cryptr.ts
+++
@@ -1,7 +0,0 @@
-import Cryptr from "cryptr";
-import dotenv from "dotenv";
-dotenv.config({ path: ".env" });
-
-const cryptr = new Cryptr(process.env.CRYPTR_KEY);
-
-export default cryptr;
--- src/repositories/userRepositories.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '../database/database.js'
-
-
-export async function findByEmail(email: string){
-
-   const user = await prisma.user.findFirst({where: {email: email}})
-   return user;
-   
-}
